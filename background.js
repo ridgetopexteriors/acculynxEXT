47f423d1812e449753583a67106e3af65e273eea
Ridge Top Exteriors <148669151+ridgetopexteriors@users.noreply.github.com>
2023-11-03 06:25:43 -0500

    Extend
    Update

--- background.js
+++ background.js
@@ -1,29 +1,119 @@
-// background.js
+// Your Axios code can now be used here
+axios.post(url, data)
+  .then(response => {
+    // Handle the response
+  })
+  .catch(error => {
+    // Handle errors
+  });
+
+// Assuming 'api' is a custom library for interacting with your API
+(require('api')('@acculynxapi/v1.0#1ypjsg71hlmi7qt7q')).getCompanySettings()
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
+
+
+(require('api')('@acculynxapi/v1.0#1ypjsg71hlmi7qt7q')).server('https://api.acculynx.com/api/v2');
+(require('api')('@acculynxapi/v1.0#1ypjsg71hlmi7qt7q')).getAppointmentById({calendarId: 'calendarId', appointmentId: 'appointmentId'})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
+
+// Set the base URL for the server
+(require('api')('@acculynxapi/v1.0#1ypjsg71hlmi7qt7q')).server('https://api.acculynx.com/api/v2/');
+
+const sdk = require('api')('@acculynxapi/v1.0#1ypjsg71hlmi7qt7q');
+
+(require('api')('@acculynxapi/v1.0#1ypjsg71hlmi7qt7q')).getUsers({pageStartIndex: '0'})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
+
+// Define the data and configuration for your POST request
+const postData = {
+  // Add your POST request data here
+};
+
+const postConfig = {
+  method: 'post', // Use 'post' method for POST requests
+  url: 'https://api.acculynx.com/api/v2/users',
+  headers: {
+    'Authorization': `Bearer ${apiKey}`
+  },
+  data: postData
+};
+
+// Make the POST request using Axios
+axios(postConfig)
+  .then(response => {
+    // Check if the POST request was successful
+    if (response.status === 200) {
+      // Your POST request was successful
+      // Now, you can make the GET request using the sdk library
+      (require('api')('@acculynxapi/v1.0#1ypjsg71hlmi7qt7q')).getAppointments({ pageStartIndex: '0', calendarId: 'calendarId' })
+        .then(({ data }) => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    } else {
+      console.error('POST request failed');
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// Additional GET Request
+(require('api')('@acculynxapi/v1.0#1ypjsg71hlmi7qt7q')).getCalendars({ pageStartIndex: '0' })
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
+
+const baseUrl = 'https://api.acculynx.com/api/v2/calendars';
+
+// Your API key
+const apiKey = 'MWE1OWFhMGEtMjlkYS00MWVmLThiZTYtZmMxNDY4YzVlMGNjN2UzNTg3M2EtMmB9ZC00NWEwLWI4NWMtMWRlN2FlODMxZDk0';
+
+// Function to fetch appointment data
+async function fetchAppointmentData(calendarId, appointmentId) {
+  try {
+    const response = await axios.get(`${baseUrl}/calendar/${calendarId}/appointment/${appointmentId}`, {
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const appointmentData = response.data;
+    console.log('Fetched Appointment Data:', appointmentData);
+
+    // You can further process the appointmentData as needed
+  } catch (error) {
+    console.error('Error fetching appointment data:', error);
+  }
+}
 
 // Listen for messages from the popup script
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.type === "scheduleFollowUp" || request.type === "performAction") {
+    // Extract data from the message
+    const followUpDate = request.followUpDate;
+    const priority = request.priority;
+    const notes = request.notes;
+
     if (request.type === "scheduleFollowUp") {
-      // Extract data from the message
-      const followUpDate = request.followUpDate;
-      const priority = request.priority;
-      const notes = request.notes;
-  
       // Implement your follow-up scheduling logic here.
       // This can include saving the follow-up data to your database, sending notifications, etc.
-  
+
       // For demonstration purposes, let's log the data.
       console.log("Follow-up Date:", followUpDate);
       console.log("Priority:", priority);
       console.log("Notes:", notes);
-  
-      // You can also send a confirmation message back to the popup script if needed.
-      sendResponse({ confirmation: "Follow-up scheduled successfully" });
     } else if (request.type === "performAction") {
       // Extract data from the message for action handling
       const selectedAction = request.selectedAction;
       const messageContent = request.messageContent;
       const recipientEmail = request.recipientEmail;
-  
+
       if (selectedAction === "message" || selectedAction === "email") {
         if (selectedAction === "message") {
           console.log(`Sending message: "${messageContent}" to ${recipientEmail}`);
@@ -32,10 +122,16 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
           console.log(`Sending email: "${messageContent}" to ${recipientEmail}`);
           // Implement email sending logic here.
         }
-  
+
         // Send a confirmation message back to the popup script if needed.
         sendResponse({ confirmation: `${selectedAction} sent successfully` });
       }
     }
-  });
-  
+  } else if (request.type === "fetchAppointmentData") {
+    // Extract calendarId and appointmentId from the request
+    const { calendarId, appointmentId } = request;
+
+    // Call the fetchAppointmentData function with the provided IDs
+    fetchAppointmentData(calendarId, appointmentId);
+  }
+});
--- popup.js
+++ popup.js
@@ -1,60 +1,140 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const performActionBtn = document.getElementById('performAction');
-    const messageContainer = document.getElementById('messageContainer');
-    const scheduleFollowUpBtn = document.getElementById('scheduleFollowUp');
-    
-    performActionBtn.addEventListener('click', function() {
-      const selectedAction = document.querySelector('input[name="action"]:checked').value;
-      
-      if (selectedAction === 'message' || selectedAction === 'email') {
-        const messageContent = document.getElementById('messageContent');
-        const recipientEmail = document.getElementById('recipientEmail');
-        
-        if (messageContent && recipientEmail) {
-          const messageText = messageContent.value;
-          const recipient = recipientEmail.value;
-          
-          if (selectedAction === 'message') {
-            console.log(`Sending message: "${messageText}" to ${recipient}`);
-            // Implement message sending logic here.
-          } else {
-            console.log(`Sending email: "${messageText}" to ${recipient}`);
-            // Implement email sending logic here.
-          }
-        }
-      } else {
-        console.log('Scheduling a follow-up.');
-        // Handle scheduling a follow-up (existing logic).
-      }
-      
-      window.close();
-    });
-    
-    scheduleFollowUpBtn.addEventListener('click', function() {
-      const followUpDate = document.getElementById('followUpDate');
-      const priority = document.getElementById('priority');
-      const notes = document.getElementById('notes');
-  
-      if (followUpDate && priority && notes) {
-        const followUpDateValue = followUpDate.value;
-        const priorityValue = priority.value;
-        const notesValue = notes.value;
-  
-        // Implement your scheduling logic here.
-        // You can use the data collected from the popup interface.
-        // For example, send this data to your background script for processing.
-  
-        // Example: Sending data to the background script.
-        chrome.runtime.sendMessage({
-          type: 'scheduleFollowUp',
-          followUpDate: followUpDateValue,
-          priority: priorityValue,
-          notes: notesValue,
-        });
-  
-        // Close the popup after scheduling.
-        window.close();
-      }
-    });
+// axios
+import axios from 'axios';
+
+// Your Axios code can now be used here
+axios.post(url, data)
+  .then(response => {
+    // Handle the response
+  })
+  .catch(error => {
+    // Handle errors
   });
-  
+
+document.addEventListener('DOMContentLoaded', function() {
+  const performActionBtn = document.getElementById('performAction');
+  const scheduleFollowUpBtn = document.getElementById('scheduleFollowUp');
+
+  // Function to handle actions (message or email sending)
+  function handleAction(selectedAction) {
+    if (selectedAction === 'message' || selectedAction === 'email') {
+      const messageContent = document.getElementById('messageContent');
+      const recipientEmail = document.getElementById('recipientEmail');
+
+      if (messageContent && recipientEmail) {
+        const messageText = messageContent.value;
+        const recipient = recipientEmail.value;
+
+        if (selectedAction === 'message') {
+          console.log(`Sending message: "${messageText}" to ${recipient}`);
+          // Make an API request to send a message using AccuLynx API
+          sendAccuLynxMessage(recipient, messageText);
+        } else {
+          console.log(`Sending email: "${messageText}" to ${recipient}`);
+          // Make an API request to send an email using AccuLynx API
+          sendAccuLynxEmail(recipient, messageText);
+        }
+      }
+    } else {
+      console.log('Scheduling a follow-up.');
+      // Handle scheduling a follow-up (existing logic).
+      // You can send data to your background script for further processing.
+    }
+
+    window.close();
+  }
+
+  // Function to send a message using AccuLynx API
+  function sendAccuLynxMessage(recipient, message) {
+    const calendarId = 'yourCalendarId'; // Set the calendar ID as needed
+    const apiKey = 'NjRmMDYxODAtYTc3Mi00ODEyLWIyZmEtODFhY2E4Yzc4OTg3YjcyMDY0YjgtOTIzMy00MGE3LTkzZWUtNTllODg2NGI1YmM3'; // Replace with your actual API key
+    const apiEndpoint = 'https://api.acculynx.com/api/v2/calendars'; // Replace with your API endpoint
+
+    // Make an API request to send a message using AccuLynx API
+    axios.post(`${apiEndpoint}/messages?calendarId=${calendarId}`, {
+      recipient: recipient,
+      message: message,
+    }, {
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      console.log('Message sent successfully:', response.data);
+    })
+    .catch(error => {
+      console.error('Error sending message:', error);
+    });
+  }
+
+  // Function to send an email using AccuLynx API
+  function sendAccuLynxEmail(recipient, emailContent) {
+    const calendarId = 'yourCalendarId'; // Set the calendar ID as needed
+    const apiKey = 'NjRmMDYxODAtYTc3Mi00ODEyLWIyZmEtODFhY2E4Yzc4OTg3YjcyMDY0YjgtOTIzMy00MGE3LTkzZWUtNTllODg2NGI1YmM3'; // Replace with your actual API key
+    const apiEndpoint = 'https://api.acculynx.com/api/v2/calendars'; // Replace with your API endpoint
+
+    // Make an API request to send an email using AccuLynx API
+    axios.post(`${apiEndpoint}/emails?calendarId=${calendarId}`, {
+      recipient: recipient,
+      content: emailContent,
+    }, {
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      console.log('Email sent successfully:', response.data);
+    })
+    .catch(error => {
+      console.error('Error sending email:', error);
+    });
+  }
+
+  // Function to schedule a follow-up using AccuLynx API
+  function scheduleAccuLynxFollowUp(date, priority, notes) {
+    const calendarId = 'yourCalendarId'; // Set the calendar ID as needed
+    const apiKey = 'NjRmMDYxODAtYTc3Mi00ODEyLWIyZmEtODFhY2E4Yzc4OTg3YjcyMDY0YjgtOTIzMy00MGE3LTkzZWU=';
+    const apiEndpoint = 'https://api.acculynx.com/api/v2/calendars';
+
+    axios.post(`${apiEndpoint}/follow-ups?calendarId=${calendarId}`, {
+      date: date,
+      priority: priority,
+      notes: notes,
+    }, {
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      console.log('Follow-up scheduled successfully:', response.data);
+    })
+    .catch(error => {
+      console.error('Error scheduling follow-up:', error);
+    });
+  }
+  // JavaScript to handle file upload
+
+// Add a change event listener to the file input element
+document.getElementById('excelFile').addEventListener('change', handleFileSelect);
+
+// Function to handle file selection
+function handleFileSelect(event) {
+  const fileInput = event.target;
+  const files = fileInput.files;
+
+  if (files.length > 0) {
+    // Handle the selected file, e.g., you can access it using files[0]
+    const selectedFile = files[0];
+    console.log('Selected file:', selectedFile.name);
+  }
+}
+
+// Add a click event listener to the upload label
+document.getElementById('uploadLabel').addEventListener('click', function () {
+  // Trigger the file input when the label is clicked
+  document.getElementById('excelFile').click();
+});
+
+});
